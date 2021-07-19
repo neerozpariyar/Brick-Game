f0cfb565f8b8d03f385e57537d9808367c7cad9e
neerozpariyar <Neeroznrz@gmail.com>
2021-07-19 21:23:33 +0545

    Brick

---
+++ src/brick.js
@@ -0,0 +1,34 @@
+import { detectCollision } from "./collisionDetection";
+
+export default class Brick {
+  constructor(game, position) {
+    this.image = document.getElementById("img_brick");
+
+    this.game = game;
+
+    this.position = position;
+
+    this.width = 64;
+
+    this.height = 24;
+
+    this.markedForDEletion = false;
+  }
+  update() {
+    if (detectCollision(this.game.ball, this)) {
+      this.game.ball.speed.y = -this.game.ball.speed.y;
+
+      this.markedForDeletion = true;
+    }
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+}
